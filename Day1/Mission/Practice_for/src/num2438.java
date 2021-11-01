c35bf54bf868990232b662b969c042de64b1a88a
jeremy0405 <81368630+jeremy0405@users.noreply.github.com>
2021-11-01 16:18:52 +0900

    mission

---
+++ Day1/Mission/Practice_for/src/num2438.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class num2438{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i = 1; i <= num; i++){
+            String star = "*";
+            System.out.println(star.repeat(i));
+        }
+    }
+}
