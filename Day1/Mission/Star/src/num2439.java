801c425ff71933718855ccf946c2b5da1a76eea6
jeremy0405 <81368630+jeremy0405@users.noreply.github.com>
2021-11-01 17:37:28 +0900

    mission

---
+++ Day1/Mission/Star/src/num2439.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class num2439{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i = 1; i <= num; i++){
+            String star = "*";
+            String space = " ";
+            System.out.println(space.repeat(num-i) + star.repeat(i));
+        }
+    }
+}
