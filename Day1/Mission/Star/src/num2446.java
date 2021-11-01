8fdbebe78837ab942f1459e69ad6a0a0af22bf62
jeremy0405 <81368630+jeremy0405@users.noreply.github.com>
2021-11-01 17:57:41 +0900

    mission

---
+++ Day1/Mission/Star/src/num2446.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class num2446 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int row = num * 2 - 1;
+
+        String star = "*";
+        String space = " ";
+
+        for(int i = 0; i < num; i++){
+            System.out.print(space.repeat(i));
+            System.out.println(star.repeat(row - 2*i));
+        }
+
+        for(int i = num-2; i >= 0; i--){
+            System.out.print(space.repeat(i));
+            System.out.println(star.repeat(row - 2*i));
+        }
+    }
+}
--- Day1/Mission/Star/src/num2447.java
+++ Day1/Mission/Star/src/num2447.java
@@ -1,12 +1,22 @@
 import java.util.Scanner;
 
 public class num2447 {
+
+    static String star = "*";
+    static String space = " ";
+
+    static void draw(int num){
+
+        System.out.print(star.repeat(num));
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        String star = "*";
-        String space = " ";
 
-        
+        for(int i = 3; i <= num; i *= 3)
+        {
+            draw(i);
+        }
     }
 }
