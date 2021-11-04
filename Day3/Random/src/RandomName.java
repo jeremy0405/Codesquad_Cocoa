aacc1ee1be39690a5e1c85a9bab3ee534af6c8bd
Jang HyungSuk <jeremy0405@naver.com>
2021-11-04 18:23:14 +0900

    랜덤 이름 뽑기 Method로 변경

--- Day3/Random/src/RandomName.java
+++ Day3/Random/src/RandomName.java
@@ -10,12 +10,7 @@ public class RandomName {
         return a;
     }
 
-    public static void main(String[] args) {
-        int num = Getnum();
-
-        String[] name = {"Jerry", "donggi", "땃쥐", "후", "Tany", "피오", "K", "taksu", "MK", "노리",
-            "mandoo"};
-
+    public static int[] getIdx(int num) {
         Random random = new Random();
 
         int[] idx = new int[num];
@@ -27,6 +22,12 @@ public class RandomName {
                 }
             }
         }
+        return idx;
+    }
+
+    public static void printName(int num, int[] idx) {
+        String[] name = {"Jerry", "donggi", "땃쥐", "후", "Tany", "피오", "K", "taksu", "MK", "노리",
+            "mandoo"};
 
         for (int i = 0; i < num; i++) {
             System.out.print(name[idx[i]]);
@@ -34,7 +35,15 @@ public class RandomName {
                 System.out.print(", ");
             }
         }
+    }
 
+    public static void main(String[] args) {
+
+        int num = Getnum();
+
+        int[] idx = getIdx(num);
+
+        printName(num, idx);
 
     }
 
