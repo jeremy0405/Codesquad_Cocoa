8accba7b0bd6047501b8542317ce0c26da4373e0
Jang HyungSuk <jeremy0405@naver.com>
2021-11-08 12:32:08 +0900

    홀짝 final update

--- Day6/holjjak/src/HoljjakGame.java
+++ Day6/holjjak/src/HoljjakGame.java
@@ -21,48 +21,14 @@ public class HoljjakGame {
 
         while (isKeepGame) {
 
-            System.out.println("User Name : " + name);
-            System.out.println("Your Money : " + PlayerMoney);
-            System.out.println("Counter Money : " + CounterMoney);
+            printStatus(name);
 
             int num = ran.nextInt(20);
             System.out.println("치트키 : " + num);
 
-            System.out.println("배팅 금액을 입력하세요!");
-            int bet = sc.nextInt();
-            if (bet > Math.min(PlayerMoney, CounterMoney)) {
-                System.out.printf("베팅 금액을 %d 이하로 입력해 주세요!\n", Math.min(PlayerMoney, CounterMoney));
-                bet = sc.nextInt();
-            }
+            int bet = getBet();
 
-            System.out.print("홀 또는 짝을 입력해주세요 : ");
-
-            sc.nextLine();
-            String input = sc.nextLine();
-
-            if (input.equals("홀")) {
-                if (num % 2 == 1) {
-                    System.out.println("Player의 승리");
-                    PlayerMoney += bet;
-                    CounterMoney -= bet;
-                } else {
-                    System.out.println("상대방의 승리");
-                    PlayerMoney -= bet;
-                    CounterMoney += bet;
-                }
-            }
-
-            if (input.equals("짝")) {
-                if (num % 2 == 0) {
-                    System.out.println("Player의 승리");
-                    PlayerMoney += bet;
-                    CounterMoney -= bet;
-                } else {
-                    System.out.println("상대방의 승리");
-                    PlayerMoney -= bet;
-                    CounterMoney += bet;
-                }
-            }
+            checkHolJjak(num, bet);
 
             if (PlayerMoney == 0) {
                 isKeepGame = false;
@@ -79,21 +45,66 @@ public class HoljjakGame {
         return true;
     }
 
+    private static void checkHolJjak(int num, int bet) {
+        System.out.print("홀 또는 짝을 입력해주세요 : ");
+
+        sc.nextLine();
+        String input = sc.nextLine();
+
+        if (input.equals("홀")) {
+            if (num % 2 == 1) {
+                System.out.println("Player의 승리");
+                PlayerMoney += bet;
+                CounterMoney -= bet;
+            } else {
+                System.out.println("상대방의 승리");
+                PlayerMoney -= bet;
+                CounterMoney += bet;
+            }
+        }
+
+        if (input.equals("짝")) {
+            if (num % 2 == 0) {
+                System.out.println("Player의 승리");
+                PlayerMoney += bet;
+                CounterMoney -= bet;
+            } else {
+                System.out.println("상대방의 승리");
+                PlayerMoney -= bet;
+                CounterMoney += bet;
+            }
+        }
+    }
+
+    private static int getBet() {
+        System.out.println("배팅 금액을 입력하세요!");
+        int bet = sc.nextInt();
+        if (bet > Math.min(PlayerMoney, CounterMoney)) {
+            System.out.printf("베팅 금액을 %d 이하로 입력해 주세요!\n", Math.min(PlayerMoney, CounterMoney));
+            bet = sc.nextInt();
+        }
+        return bet;
+    }
+
+    private static void printStatus(String name) {
+        System.out.println("User Name : " + name);
+        System.out.println("Your Money : " + PlayerMoney);
+        System.out.println("Counter Money : " + CounterMoney);
+    }
+
     private static void gameRun(String name) {
         boolean KeepGame = true;
         int Round = 1;
         while (KeepGame) {
             System.out.println("Round " + Round);
             KeepGame = gameStart(name);
-//            CounterMoney = (int) (PlayerMoney * 1.2);
-            CounterMoney = 100;
             Round++;
             if(Round == 9) {
                 break;
             }
+            CounterMoney = (int) (PlayerMoney * Math.pow(1.2, Round));
         }
     }
-
 
     public static void main(String[] args) throws IOException {
 
