9ba37e37fe3cb93a95861e17e1a2efb95acf6f8f
Jang HyungSuk <jeremy0405@naver.com>
2021-11-10 12:37:52 +0900

    홀짝 게임 RankerBoard 구현
    ArrayList를 만들어 add 할때부터 정렬해서 add를 해줌으로써 정렬된
    ArrayList들을 생성했음.

--- Day6/holjjak/src/HoljjakGame.java
+++ Day6/holjjak/src/HoljjakGame.java
@@ -1,15 +1,22 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class HoljjakGame {
 
-    public static int PlayerMoney = 100;
-    public static int CounterMoney = 120;
+    public static int PlayerMoney;
+    public static int CounterMoney;
+    public static int MaxPlayerMoney;
+    public static int turn;
+    public static ArrayList<String> RankName = new ArrayList<>();
+    public static ArrayList<Integer> RankMoney = new ArrayList<>();
+    public static ArrayList<Integer> RankTurn = new ArrayList<>();
     public static Scanner sc = new Scanner(System.in);
 
     private static String getPlayerName() {
-        System.out.println("이름을 입력하세요!");
+        System.out.print("이름을 입력하세요! >>>>> ");
         String playerName = sc.nextLine();
         return playerName;
     }
@@ -17,10 +24,8 @@ public class HoljjakGame {
     public static boolean gameStart(String name) {
         Random ran = new Random();
 
-        boolean isKeepGame = true;
-
-        while (isKeepGame) {
-
+        while (true) {
+            turn++;
             printStatus(name);
 
             int num = ran.nextInt(20);
@@ -31,18 +36,20 @@ public class HoljjakGame {
             checkHolJjak(num, bet);
 
             if (PlayerMoney == 0) {
-                isKeepGame = false;
+
                 System.out.printf("Round 종료!!\n\n\n\n");
+
+                printResult(name);
+
                 return false;
             }
 
             if (CounterMoney == 0) {
-                isKeepGame = false;
+
                 System.out.printf("Round 종료!!\n\n\n\n");
                 return true;
             }
         }
-        return true;
     }
 
     private static void checkHolJjak(int num, int bet) {
@@ -53,11 +60,12 @@ public class HoljjakGame {
 
         if (input.equals("홀")) {
             if (num % 2 == 1) {
-                System.out.println("Player의 승리");
+                System.out.println("Player의 승리\n\n");
                 PlayerMoney += bet;
+                MaxPlayerMoney = Math.max(PlayerMoney, MaxPlayerMoney);
                 CounterMoney -= bet;
             } else {
-                System.out.println("상대방의 승리");
+                System.out.println("상대방의 승리\n\n");
                 PlayerMoney -= bet;
                 CounterMoney += bet;
             }
@@ -65,11 +73,12 @@ public class HoljjakGame {
 
         if (input.equals("짝")) {
             if (num % 2 == 0) {
-                System.out.println("Player의 승리");
+                System.out.println("Player의 승리\n\n");
                 PlayerMoney += bet;
+                MaxPlayerMoney = Math.max(PlayerMoney, MaxPlayerMoney);
                 CounterMoney -= bet;
             } else {
-                System.out.println("상대방의 승리");
+                System.out.println("상대방의 승리\n\n");
                 PlayerMoney -= bet;
                 CounterMoney += bet;
             }
@@ -77,10 +86,10 @@ public class HoljjakGame {
     }
 
     private static int getBet() {
-        System.out.println("배팅 금액을 입력하세요!");
+        System.out.print("배팅 금액을 입력하세요! >>>>> ");
         int bet = sc.nextInt();
         if (bet > Math.min(PlayerMoney, CounterMoney)) {
-            System.out.printf("베팅 금액을 %d 이하로 입력해 주세요!\n", Math.min(PlayerMoney, CounterMoney));
+            System.out.printf("베팅 금액을 %d 이하로 입력해 주세요! >>>>> ", Math.min(PlayerMoney, CounterMoney));
             bet = sc.nextInt();
         }
         return bet;
@@ -90,27 +99,109 @@ public class HoljjakGame {
         System.out.println("User Name : " + name);
         System.out.println("Your Money : " + PlayerMoney);
         System.out.println("Counter Money : " + CounterMoney);
+        System.out.println("turn : " + turn);
     }
 
-    private static void gameRun(String name) {
+    private static boolean gameRun(String name) {
         boolean KeepGame = true;
         int Round = 1;
+        PlayerMoney = 100;
+        CounterMoney = 120;
+        MaxPlayerMoney = 100;
+        turn = 0;
         while (KeepGame) {
             System.out.println("Round " + Round);
             KeepGame = gameStart(name);
             Round++;
-            if(Round == 9) {
+            if (Round == 9) {
+                printResult(name);
                 break;
             }
             CounterMoney = (int) (PlayerMoney * Math.pow(1.2, Round));
         }
+
+        return vaildation();
+
     }
 
-    public static void main(String[] args) throws IOException {
+    private static boolean vaildation() {
+        System.out.print("게임을 더 진행하시겠습니까? (Y/N) >>>>> ");
+        String YesNo = sc.nextLine();
 
-        String name = getPlayerName();
+        if (YesNo.equals("Y")) {
+            return true;
+        } else if (YesNo.equals("N")) {
+            return false;
+        } else {
+            System.out.println("잘 못 입력하셨습니다. 다시 입력해주세요!");
+            return vaildation();
+        }
+    }
 
-        gameRun(name);
+    private static void printResult(String name) {
+
+        getUserData(name);
+
+        System.out.println("====================RankerBoard======================");
+        for (int i = 0; i < RankName.size(); i++) {
+            System.out.printf("User Name : %8s \t Max Money : %5d \t Turn : %3d\n", RankName.get(i),
+                RankMoney.get(i), RankTurn.get(i));
+        }
+        System.out.println("=====================================================");
+        System.out.println("\n\n\n\n\n\n");
+
+    }
+
+    private static void getUserData(String name) {
+        if (RankName.size() == 0) {
+            RankName.add(name);
+            RankMoney.add(MaxPlayerMoney);
+            RankTurn.add(turn);
+        } else {
+            int idx;
+            for (int i = 0; i < RankMoney.size(); i++) {
+                if (MaxPlayerMoney > RankMoney.get(i)) {
+                    idx = i;
+                    RankName.add(idx, name);
+                    RankMoney.add(idx, MaxPlayerMoney);
+                    RankTurn.add(idx, turn);
+                    break;
+                } else if (MaxPlayerMoney == RankMoney.get(i)) {
+                    for (int j = 0; j < RankTurn.size(); j++) {
+                        if (turn > RankTurn.get(j)) {
+                            idx = j;
+                            RankName.add(idx, name);
+                            RankMoney.add(idx, MaxPlayerMoney);
+                            RankTurn.add(idx, turn);
+                            break;
+                        } else {
+                            RankName.add(name);
+                            RankMoney.add(MaxPlayerMoney);
+                            RankTurn.add(turn);
+                            break;
+                        }
+                    }
+                    break;
+                } else {
+                    RankName.add(name);
+                    RankMoney.add(MaxPlayerMoney);
+                    RankTurn.add(turn);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        boolean isKeepGame = true;
+
+        while (isKeepGame) {
+            String name = getPlayerName();
+            isKeepGame = gameRun(name);
+        }
+
+
     }
 
 }
