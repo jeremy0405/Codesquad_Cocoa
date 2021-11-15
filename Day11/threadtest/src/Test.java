67872f55e4d8a1445e60e2a030f4d494fd95d53f
Jang HyungSuk <jeremy0405@naver.com>
2021-11-15 15:25:00 +0900

    스레드 테스트

--- Day11/threadtest/src/Test.java
+++ Day11/threadtest/src/Test.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Test {
+public class Test extends Thread {
+    int seq;
+    public Test(int seq) {
+        this.seq = seq;
+    }
+    public void run() {
+        System.out.println(this.seq+" thread start.");
+        try {
+            Thread.sleep(20000);
+        }catch(Exception e) {
+
+        }
+        System.out.println(this.seq+" thread end.");
+    }
+
+    public static void main(String[] args) {
+        for(int i=0; i<10; i++) {
+            Thread t = new Test(i);
+            t.start();
+        }
+        System.out.println("main end.");
+    }
 }
