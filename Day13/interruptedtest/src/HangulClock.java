fd447efebb188f0f00d4dcc25405f08ec47dc2cc
Jang HyungSuk <jeremy0405@naver.com>
2021-11-17 17:22:09 +0900

    한글시계&달력 구현완료
    인터럽트를 사용해 시계와 달력간 메뉴이동이 가능하게 설계
    메인 메소드가 더럽다. 해결 방법은 모르겠음.. 내 수준 밖인듯?

---
+++ Day13/interruptedtest/src/HangulClock.java
@@ -0,0 +1,32 @@
+import java.time.LocalTime;
+
+public class HangulClock {
+
+    private int hour;
+    private int minute;
+    LocalTime now = LocalTime.now();
+
+    HangulMap hangulMap = new HangulMap();
+
+    public void clockRun() {
+        setHour();
+        setMinute();
+
+        hangulMap.setMap(this.hour, this.minute);
+        String[][] map = hangulMap.getMap();
+
+        System.out.printf("%n%n%n%n%n%n%n%n%n%n%n%d: %02d%n%n",
+            this.hour, this.minute);
+
+        hangulMap.printMap(map);
+    }
+
+    public void setHour() {
+        this.hour = now.getHour();
+    }
+
+    public void setMinute() {
+        this.minute = now.getMinute();
+    }
+
+}
--- Day13/interruptedtest/src/Interrupted.java
+++ Day13/interruptedtest/src/Interrupted.java
@@ -1,2 +1,84 @@
-package PACKAGE_NAME;public class Main {
+import static java.lang.Thread.sleep;
+
+import java.util.Scanner;
+
+public class Interrupted implements Runnable {
+
+    public void run() {
+        try {
+            while (true) {
+                HangulClock hangulClock = new HangulClock();
+                hangulClock.clockRun();
+                sleep(10000); // 10초마다 갱신.
+            }
+        } catch (Exception e) {
+            //TODO 메뉴로 이동
+        }
+
+    }
+
+    public static void main(String[] args) {
+        boolean isWork = true;
+        while (isWork) {
+            isWork = systemRun();
+        }
+    }
+
+    private static boolean systemRun() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("clock or cal >> ");
+        String command = sc.nextLine();
+        return choiceOption(command);
+    }
+
+    private static boolean choiceOption(String command) {
+        if (command.equals("clock")) {
+            Interrupted t = new Interrupted();
+            Thread thread = new Thread(t);
+            thread.start();
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                String s = sc.nextLine();
+                if (s.equals("exit")) {
+                    thread.interrupt();
+                    break;
+                }
+            }
+            return true;
+        } else if (command.equals("cal")) {
+            calendar();
+            return true;
+        } else if (command.equals("exit")) {
+            return false;
+        } else {
+            systemRun();
+            return false;
+        }
+    }
+
+    private static void calendar() {
+        Scanner sc = new Scanner(System.in);
+        Calendar calendar = new Calendar();
+        calendar.printCal();
+        calMenu(sc);
+    }
+
+    private static void calMenu(Scanner sc) {
+        Calendar calendar;
+        boolean isWork = true;
+        while (isWork) {
+            try {
+                System.out.print("년, 월을 입력해주세요 ex) 2021 10\n※올바르지 않은 값 입력시 메뉴로 이동 >> ");
+                int year = sc.nextInt();
+                int month = sc.nextInt();
+                sc.nextLine();
+                calendar = new Calendar(year, month);
+                calendar.printCal();
+            } catch (Exception e) {
+                System.out.println("메뉴로 돌아갑니다.");
+                isWork = false;
+            }
+        }
+    }
+
 }
