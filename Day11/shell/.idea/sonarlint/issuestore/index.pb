ac6110ad9256d4f70a81bb54a2b30de137e12d52
Jang HyungSuk <jeremy0405@naver.com>
2021-11-19 16:26:45 +0900

    shell 구현 완료
    시계, pwd cd dir copy mkdir rmdir 등 구현 완료
    pwd cd는 직접 구현
    copy mkdir rmdir 등은 ProcessBuilder 사용
    초기에 Process Runtime.exec() 통해서 구현했으나 path 설정이 어려워
    ProcessBuilder로 변경.

--- Day11/shell/.idea/sonarlint/issuestore/index.pb
+++ Day11/shell/.idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,13 @@ I
 src/hclock/Mainclock.java,5\b\5ba3f91fd97c8c48742704316c7bea6031caaa8c
 D
 src/hclock/Menu.java,8\b\8b13ea159356878cfdf455413b1d8f3523abf11f
+B
+src/MainShell.java,2\2\22cb258098298ba19e91eeca8bce808189e46a66
+>
+src/Shell.java,7\d\7d7f27f1cb889ffa4d42cf7b3ac01060cc5541bc
+@
+src/CmdMenu.java,e\9\e9431145a3acc5c98cb6f0afad82b0f9d9a4058f
+>
+src/Input.java,2\1\2102397d7dd165704213c3a0455651b5f8131c30
+<
+src/Cmd.java,5\c\5cc44e9bf2d815a9dbeb1f7084880fa5a26f014d
--- Day11/shell/src/Cmd.java
+++ Day11/shell/src/Cmd.java
@@ -1,13 +1,26 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Cmd {
 
     private Path path = Paths.get("").toAbsolutePath();
 
     public void cd(String s) {
+        if (s.equals("..")) {
+            path = path.getParent();
+            System.out.println(path.toString());
+        } else if (s.equals("~")) {
+            path = path.getRoot();
+            System.out.println(path.toString());
+        } else {
+            path = Paths.get(path.toString() + s);
+            System.out.println(path);
+        }
 
     }
 
@@ -18,11 +31,20 @@ public class Cmd {
 
     public void cmd(String command) {
         try {
-            Process p = Runtime.getRuntime().exec("cmd /c " + command);
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.directory(new File(path.toString()));
+            processBuilder.redirectErrorStream(true);
+            ArrayList<String> commandlist = new ArrayList<>();
+            commandlist.add("cmd");
+            commandlist.add("/c");
+            String[] comArr = command.split(" +");
+            commandlist.addAll(Arrays.asList(comArr));
+            processBuilder.command(commandlist);
+            Process p = processBuilder.start();
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
             p.waitFor();
 
-            String line = null;
+            String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
