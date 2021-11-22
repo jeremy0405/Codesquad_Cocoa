159e657f30803a035a9a6c07f768ec2a53be448b
Jang HyungSuk <jeremy0405@naver.com>
2021-11-22 11:02:09 +0900

    awt 공부

--- Day16/awt/src/main/java/FrameTest.java
+++ Day16/awt/src/main/java/FrameTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class FrameTest {
+import java.awt.*;
+
+public class FrameTest {
+
+    public static void main(String[] args) {
+        Frame f = new Frame("Login");
+        f.setSize(300,200);
+
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenSize = tk.getScreenSize();
+
+        Button b = new Button("메 뉴");
+        b.setSize(100, 50);
+
+        f.add(b);
+        f.setLocation(screenSize.width/2 - 150, screenSize.height/2 - 100);
+        f.setVisible(true);
+    }
+
 }
