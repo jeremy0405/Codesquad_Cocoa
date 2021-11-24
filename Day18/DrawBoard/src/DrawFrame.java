ae77119d1c97b90693a5b28587450d26d6633774
Jang HyungSuk <jeremy0405@naver.com>
2021-11-24 15:32:20 +0900

    그림판 구현 완료.
    TODOLIST
    선 굵기 두껍게 변경하는중

--- Day18/DrawBoard/src/DrawFrame.java
+++ Day18/DrawBoard/src/DrawFrame.java
@@ -1,31 +1,25 @@
-import java.awt.Button;
+import java.awt.BasicStroke;
 import java.awt.Checkbox;
 import java.awt.CheckboxGroup;
-import java.awt.CheckboxMenuItem;
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
-import java.awt.Menu;
-import java.awt.MenuBar;
-import java.awt.MenuItem;
+import java.awt.Graphics2D;
 import java.awt.Panel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import javax.swing.ButtonGroup;
 
 public class DrawFrame extends Frame {
-
     private int x1;
     private int y1;
     private int x2;
     private int y2;
-
+    private static int dx1;
+    private static int dy1;
     private boolean isBlack = true;
     private boolean isRed = false;
     private boolean isBlue = false;
@@ -46,12 +40,22 @@ public class DrawFrame extends Frame {
         });
         setCheckbox();
         addMouseListener(new MouseGetter());
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent me) {
+                if (isCuv) {
+                    dx1 = me.getX();
+                    dy1 = me.getY();
+                    repaint();
+                }
+            }
+        });
 
         setVisible(true);
     }
 
+    @Override
     public void paint(Graphics g) {
-
         if (isBlack) {
             g.setColor(Color.black);
         }
@@ -72,15 +76,14 @@ public class DrawFrame extends Frame {
             g.drawOval(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
         }
         if (isCuv) {
-
+            g.fillOval(dx1 - 5, dy1 - 5, 10, 10);
         }
         if (isEraser) {
             g.clearRect(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
         }
-
-
     }
 
+    @Override
     public void update(Graphics g) {
         paint(g);
     }
@@ -94,13 +97,13 @@ public class DrawFrame extends Frame {
         Checkbox rec = new Checkbox("사각형", figuregroup, false);
         Checkbox cir = new Checkbox("원", figuregroup, false);
         Checkbox cuv = new Checkbox("곡선", figuregroup, false);
-        Checkbox eraswer = new Checkbox("지우개", figuregroup, false);
+        Checkbox eraser = new Checkbox("지우개", figuregroup, false);
 
         line.addItemListener(new FigureHandler());
         rec.addItemListener(new FigureHandler());
         cir.addItemListener(new FigureHandler());
         cuv.addItemListener(new FigureHandler());
-        eraswer.addItemListener(new FigureHandler());
+        eraser.addItemListener(new FigureHandler());
 
         Panel color = new Panel();
         color.setBackground(Color.lightGray);
@@ -117,7 +120,7 @@ public class DrawFrame extends Frame {
         figure.add(rec);
         figure.add(cir);
         figure.add(cuv);
-        figure.add(eraswer);
+        figure.add(eraser);
 
         color.add(black);
         color.add(red);
@@ -129,15 +132,12 @@ public class DrawFrame extends Frame {
     }
 
     private class ColorHandler implements ItemListener {
-
         @Override
         public void itemStateChanged(ItemEvent e) {
-            //TODO 색깔 지정 해주는 기능 추가해야함.
             if (e.getItem().equals("검은색")) {
                 isBlack = true;
                 isRed = false;
                 isBlue = false;
-                System.out.println(e.getItem());
             }
             if (e.getItem().equals("빨간색")) {
                 isBlack = false;
@@ -153,18 +153,14 @@ public class DrawFrame extends Frame {
     }
 
     private class FigureHandler implements ItemListener {
-
         @Override
         public void itemStateChanged(ItemEvent e) {
-
-            //TODO 도형 지정 해주는 기능 추가해야함.
             if (e.getItem().equals("직선")) {
                 isLine = true;
                 isRect = false;
                 isCir = false;
                 isCuv = false;
                 isEraser = false;
-                System.out.println(e.getItem());
             }
             if (e.getItem().equals("사각형")) {
                 isLine = false;
@@ -198,21 +194,16 @@ public class DrawFrame extends Frame {
     }
 
     private class MouseGetter extends MouseAdapter {
-
         @Override
         public void mousePressed(MouseEvent e) {
             x1 = e.getX();
             y1 = e.getY();
         }
-
         @Override
         public void mouseReleased(MouseEvent e) {
             x2 = e.getX();
             y2 = e.getY();
-
             repaint();
-
-            System.out.printf("%5d%5d%5d%5d\n", x1, y1, x2, y2);
         }
     }
 }
