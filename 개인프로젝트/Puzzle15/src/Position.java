07c6fbbbef800cb455dab73190d8d6110620faec
Jang HyungSuk <jeremy0405@naver.com>
2021-11-29 10:44:16 +0900

    빈 칸 x,y 좌표값 가진 Position 구현

--- 개인프로젝트/Puzzle15/src/Position.java
+++ 개인프로젝트/Puzzle15/src/Position.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Position {
+public class Position {
+    private int x;
+    private int y;
+
+    Position(){
+        this.x = 3;
+        this.y = 3;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
