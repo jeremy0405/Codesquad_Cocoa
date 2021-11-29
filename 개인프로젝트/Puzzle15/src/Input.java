53522f806f8181f6ccece3f003faa42be9006f63
Jang HyungSuk <jeremy0405@naver.com>
2021-11-29 11:08:14 +0900

    Input 클래스 소문자입력으로 처리
    close() 추가,
    소문자로만 처리하도록 변경

--- 개인프로젝트/Puzzle15/src/Input.java
+++ 개인프로젝트/Puzzle15/src/Input.java
@@ -7,13 +7,17 @@ public class Input {
     public String input() {
         System.out.print("w a s d 중 하나를 입력해주세요 >> ");
         String input = sc.nextLine();
+        input = input.toLowerCase();
 
-        if (!input.equalsIgnoreCase("w") && !input.equalsIgnoreCase("a")
-            && !input.equalsIgnoreCase("s") && !input.equalsIgnoreCase("d")) {
+        if (!input.equals("w") && !input.equals("a") && !input.equals("s") && !input.equals("d")) {
             System.out.println("잘못된 값을 입력했습니다. w a s d 중에 하나를 입력해주세요.");
             input = input();
         }
         return input;
+    }
+
+    public void close() {
+        sc.close();
     }
 
 }
