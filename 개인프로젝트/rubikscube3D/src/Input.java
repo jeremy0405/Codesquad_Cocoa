47ca7f5b92a731a090449a97669b4d689626c20a
Jang HyungSuk <jeremy0405@naver.com>
2021-12-01 12:18:24 +0900

    #6 올바른 Input만 받도록 함
    올바른 input만 받고 옳지 않은 command 입력 시 다시 input 받도록 함
    콘솔에 Cube> 및 잘못된 입력값이라고 표시해주도록 함.
    input class 구현 끝!

--- 개인프로젝트/rubikscube3D/src/Input.java
+++ 개인프로젝트/rubikscube3D/src/Input.java
@@ -7,12 +7,43 @@ public class Input {
     private Scanner sc = new Scanner(System.in);
 
     public String[] userInput() {
+        System.out.print("Cube> ");
         String userinput = sc.nextLine();
-        String[] command = validateInput(userinput);
+        String[] splitCommand = splitInput(userinput);
+        String[] command = validateInput(splitCommand);
         return command;
     }
 
-    private String[] validateInput(String userinput) {
+    private String[] validateInput(String[] command) {
+        for (int i = 0; i < command.length; i++) {
+            if (!command[i].equalsIgnoreCase("F") &&
+                !command[i].equalsIgnoreCase("F'") &&
+                !command[i].equalsIgnoreCase("F2") &&
+                !command[i].equalsIgnoreCase("R") &&
+                !command[i].equalsIgnoreCase("R'") &&
+                !command[i].equalsIgnoreCase("R2") &&
+                !command[i].equalsIgnoreCase("U") &&
+                !command[i].equalsIgnoreCase("U'") &&
+                !command[i].equalsIgnoreCase("U2") &&
+                !command[i].equalsIgnoreCase("B") &&
+                !command[i].equalsIgnoreCase("B'") &&
+                !command[i].equalsIgnoreCase("B2") &&
+                !command[i].equalsIgnoreCase("L") &&
+                !command[i].equalsIgnoreCase("L'") &&
+                !command[i].equalsIgnoreCase("L2") &&
+                !command[i].equalsIgnoreCase("D") &&
+                !command[i].equalsIgnoreCase("D'") &&
+                !command[i].equalsIgnoreCase("D2") &&
+                !command[i].equalsIgnoreCase("Q")
+            ) {
+                System.out.println("잘못된 입력값이 포함되어 있습니다. 다시 입력해주세요!");
+                return userInput();
+            }
+        }
+        return command;
+    }
+
+    private String[] splitInput(String userinput) {
         List<String> commandlist = new LinkedList<>();
         for (int i = 0; i < userinput.length(); i++) {
             if (i != 0 && userinput.charAt(i) == '2' || userinput.charAt(i) == '\'') {
@@ -23,6 +54,10 @@ public class Input {
             commandlist.add(userinput.charAt(i) + "");
         }
         return commandlist.toArray(new String[0]);
+    }
+
+    public void close() {
+        sc.close();
     }
 
 }
